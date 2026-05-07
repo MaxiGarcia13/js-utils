06352544ed80dcd6cc5ebb9480dc9a58743a6489
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-07 10:02:24 +0200

    test: Add unit tests for class name concatenation utility function

---
+++ src/classes.test.ts
@@ -0,0 +1,12 @@
+import { expect, it } from 'vitest';
+import { cn } from './classes';
+
+it('cn', () => {
+  expect(cn('foo', 'bar', 'baz')).toBe('foo bar baz');
+  expect(cn('foo', 'bar', 'baz', false)).toBe('foo bar baz');
+  expect(cn('foo', 'bar', 'baz', null)).toBe('foo bar baz');
+  expect(cn('foo', 'bar', 'baz', undefined)).toBe('foo bar baz');
+  expect(cn('foo', 'bar', 'baz', 0)).toBe('foo bar baz');
+  expect(cn('foo', 'bar', 'baz', 1)).toBe('foo bar baz 1');
+  expect(cn('foo', 'bar', 'baz', true)).toBe('foo bar baz');
+});

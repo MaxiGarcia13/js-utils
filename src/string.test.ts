8396e6d1efe290f52d89171777cc211e1d613c8e
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-05-15 01:09:08 +0200

    feat: Add unwrapString function and corresponding tests for string manipulation

--- src/string.test.ts
+++ src/string.test.ts
@@ -1,9 +1,17 @@
 import { expect, it } from 'vitest';
-import { capitalize } from './string.js';
+import { capitalize, unwrapString } from './string.js';
 
 it('capitalize', () => {
   expect(capitalize('hello')).toBe('Hello');
   expect(capitalize('HELLO')).toBe('Hello');
   expect(capitalize('hELLO')).toBe('Hello');
   expect(capitalize('')).toBe('');
+});
+
+it('unwrapString', () => {
+  expect(unwrapString('"hello"')).toBe('hello');
+  expect(unwrapString('\'hello\'')).toBe('hello');
+  expect(unwrapString('hello')).toBe('hello');
+  expect(unwrapString(`"hello"`)).toBe('hello');
+  expect(unwrapString(`'"hello"'`)).toBe('hello');
 });
--- src/string.ts
+++ src/string.ts
@@ -5,3 +5,9 @@ export function capitalize(value: string) {
 
   return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
 }
+
+export function unwrapString(value: string) {
+  let unwrapped = value;
+  while (/^['"].*['"]$/.test(unwrapped)) unwrapped = unwrapped.slice(1, -1);
+  return unwrapped;
+}
